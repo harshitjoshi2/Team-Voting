d4e778ac7e1560d02de3c4788fddd18bd28b21cd
harshitjoshi2 <harshitj547@gmail.com>
2022-05-14 11:28:13 +0530

    changemade

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDmbQxXudO0F7N9h2eWmhQJdhmLAbc4AAw",
+    authDomain: "voting-app-c070e.firebaseapp.com",
+    databaseURL: "https://voting-app-c070e-default-rtdb.firebaseio.com",
+    projectId: "voting-app-c070e",
+    storageBucket: "voting-app-c070e.appspot.com",
+    messagingSenderId: "201684240623",
+    appId: "1:201684240623:web:b663a68713f35844adf31a"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
